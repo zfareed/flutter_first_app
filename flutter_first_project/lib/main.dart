0bd290061f9fe63d85534f5da010205d0d87efed
Zain Fareed <zainfareed093@gmail.com>
2021-04-01 21:14:24 +0500

    created basic UI

--- flutter_first_project/lib/main.dart
+++ flutter_first_project/lib/main.dart
@@ -4,11 +4,16 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.lightBlue,
         appBar: AppBar(
           title: Text('My First App'),
-          backgroundColor: Colors.orange,
-          ),
+          backgroundColor: Colors.orange),
+        body: Center(
+          child: Image(
+            image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/0/0f/Fawad_khan_at_grazia_young_faishon_awards.jpg'),),
+        )
+        ),
         ), 
-  ));
+  );
 }
 
